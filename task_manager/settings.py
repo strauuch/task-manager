2c016bae0aa28118d575f35ae1b3566e58f936a5
Vadym <strauuch@gmail.com>
2025-07-25 15:25:08 +0200

    feat models: create models.py, add user_model to settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "tasks"
+    "tasks",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +121,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'tasks.Worker'
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,66 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class TaskType(models.Model):
+    name = models.CharField(max_length=155)
+
+
+    class Meta:
+        verbose_name = "Task Type"
+        verbose_name_plural = "Task Types"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Priority(models.TextChoices):
+    LOW = 'low', 'Low'
+    MEDIUM = 'medium', 'Medium'
+    HIGH = 'high', 'High'
+    CRITICAL = 'critical', 'Critical'
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=155)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_complete = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=10,
+        choices=Priority.choices,
+        default=Priority.MEDIUM
+    )
+    task_type = models.ForeignKey(TaskType, on_delete=models.SET_NULL, null=True)
+    assignee = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="assigned_tasks")
+
+    class Meta:
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
+        ordering = ['deadline']
+
+    def __str__(self):
+        return f"Task {self.name}, priority: {self.priority}, deadline: {self.deadline}, "
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=155)
+
+    class Meta:
+        verbose_name = "Position"
+        verbose_name_plural = "Positions"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        ordering = ['position__name']
+
+    def __str__(self):
+        return f"{self.position.name if self.position else ''} {self.first_name} {self.last_name}"
