ea888f418bfd3b5ea326d0b5727ea0d17243a582
Vadym <strauuch@gmail.com>
2025-08-01 13:56:28 +0200

    feat: add admin panel, add Task, TaskType, Position, Worker models to admin panel

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Task, TaskType, Position, Worker
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "priority", "deadline", "is_complete")
+    list_filter = ("priority", "is_complete", "deadline")
+    search_fields = ("name", "description")
+    filter_horizontal = ("assignee",)
+    autocomplete_fields = ("task_type",)
+
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+
+    list_display = UserAdmin.list_display + ("position",)
+    list_filter = ("position",)
+    search_fields = ("username", "first_name", "last_name")
+    autocomplete_fields = ("position",)
+
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("position",)}),)
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            None,
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "position",
+                )
+            },
+        ),
+    )
